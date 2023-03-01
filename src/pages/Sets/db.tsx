da17e5afca7a4a2f316760c6c91c8b3bcc417000
sultan <sultanhasanov987@gmail.com>
2023-03-01 09:57:02 +0300

    Добавил товар в корзину

--- src/pages/Sets/db.tsx
+++ src/pages/Sets/db.tsx
@@ -10,70 +10,70 @@ import photo2 from '../../assets/img/photo2.png'
 // }
 
 export const products: any = [
-    {
+    {   id: 1,
         image: photo,
         name: 'Саломон сет',
         weight: 1050,
         pieces: 30,
         price: 1500
     },
-    {
+    {   id: 2,
         image: photo2,
         name: 'Филадельфия Love сет',
         weight: 1000,
         pieces: 40,
         price: 1479
     },
-    {
+    {   id: 3,
         image: photo,
         name: 'Саломон сет',
         weight: 1050,
         pieces: 30,
         price: 1500
     },
-    {
+    {   id: 4,
         image: photo2,
         name: 'Филадельфия Love сет',
         weight: 1000,
         pieces: 40,
         price: 1479
     },
-    {
+    {   id: 5,
         image: photo,
         name: 'Саломон сет',
         weight: 1050,
         pieces: 30,
         price: 1500
     },
-    {
+    {   id: 6,
         image: photo2,
         name: 'Филадельфия Love сет',
         weight: 1000,
         pieces: 40,
         price: 1479
     },
-    {
+    {   id: 7,
         image: photo,
         name: 'Саломон сет',
         weight: 1050,
         pieces: 30,
         price: 1500
     },
-    {
+    {   id: 8,
         image: photo2,
         name: 'Филадельфия Love сет',
         weight: 1000,
         pieces: 40,
         price: 1479
     },
-    {
+    {   id: 9,
         image: photo,
         name: 'Саломон сет',
         weight: 1050,
         pieces: 30,
         price: 1500
     },
-    {
+    {   id: 10,
         image: photo2,
         name: 'Филадельфия Love сет',
         weight: 1000,
