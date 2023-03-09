f26439a9871a910ab2b7e9ade1b6696277217b4e
Martazanov <saidmartazanov1@gmail.com>
2023-03-09 12:44:41 +0300

    header fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import './App.css';
 import Basket from './pages/Basket/Basket';
-// import MainPage from './pages/MainPage/MainPage';
-// import Sets from './pages/Sets/Sets';
 import { Routes, Route, Link } from 'react-router-dom';
 import Sets from './pages/Sets/Sets';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
-      <Link to="/sets">Sets</Link>
+      {/* <Link to="/sets">Sets</Link>
       <Link to="/basket">Basket</Link>
 
       <Routes>
         <Route path="/sets" element={<Sets />} />
         <Route path="/basket" element={<Basket />} />
-      </Routes>
+      </Routes> */}
+      <Header />
     </>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-
+import styles from './Header.module.scss';
 const Header = () => {
   return (
     <header>
-      <div className="container">
-        <div style={{ display: 'flex' }} className="content">
-          <div className="logo">
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.logo}>
             <img src="./assets/logo.svg" alt="Logo_sushi" />
           </div>
-          <div className="contact">
-            <span>Наш телефон</span>
-            <div className="number-1">89888235658</div>
-            <div className="number-2">89286953059</div>
+          <div className={styles.contact}>
+            <span>Наш телефон:</span>
+            <div className={styles.numberPhone}>89888235658</div>
+            <div className={styles.numberPhone}>89286953059</div>
           </div>
-          <div className="header-time">
+          <div className={styles.headerTime}>
             <img src="./assets/headerTime.svg" alt="time" />
-            <div className="txt">
+            <div className={styles.txt}>
               {' '}
               <div>работаем с </div>
               <div>10:00 до 00:00 </div>
             </div>
           </div>
-          <div className="title-search">
+          <div className={styles.titleSearch}>
             <img src="./assets/Search.svg" alt="Search" />
           </div>
         </div>
