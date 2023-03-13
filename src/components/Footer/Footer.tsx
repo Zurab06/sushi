b840f8ab0b13442ecb3443e1f6811444ca91cdb7
Martazanov <saidmartazanov1@gmail.com>
2023-03-13 08:57:23 +0300

    update

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+const Footer = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="txt"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
