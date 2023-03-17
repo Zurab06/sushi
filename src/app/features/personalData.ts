c8a9342f9f96f316ff7a7e0d46268fd3b8ddb391
Said <saidmartazanov1@mail.ru>
2023-03-17 22:00:07 +0300

    axios reducer: personalData

--- src/app/features/personalData.ts
+++ src/app/features/personalData.ts
@@ -1,8 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from '../../axios';
 
 export interface PersonalData {
-  dataUser: [] | null;
+  dataUser: [];
 }
+export type PersonalDataType = {
+  surname: string;
+  name: string;
+  street: string;
+  flat: string;
+  entrance: string;
+  floor: string;
+  code: string;
+  cash: string;
+  oddMoney: string;
+  sum: string;
+  email: string;
+  comment: string;
+};
+const createPersonalData = createAsyncThunk(
+  'personalData/createPersonalData',
+  async (data: PersonalDataType, thunkApi) => {
+    const user = await axios;
+  },
+);
 
 const initialState: PersonalData = {
   dataUser: [],
