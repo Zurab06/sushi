2fed883986b2834c3977fb25b6203758dd8b9adc
Said <saidmartazanov1@mail.ru>
2023-03-23 23:41:46 +0300

    react-hook-form

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Login" />
+      <input type="text" placeholder="Password" />
+    </div>
+  );
+};
+
+export default Login;
